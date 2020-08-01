6d1e7d906d264cce8ae8f0b90a7549c1162b600d
Reis <reisfmr@decea.gov.br>
2020-08-01 19:05:37 -0300

    First commit

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Home msg="Bem vindo ao YouTube Trailers"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Home from '@/components/Home.vue';
+//import axios from "axios"
+
+export default {
+  methods: {
+  },
+  name: 'Home',
+  components: {
+    Home
+  }
+}
+</script>
